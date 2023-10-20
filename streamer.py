5033ea02a3cce79d2e90cb71fa7323ff94022f76
Jordan Checkoff <jordan.checkoff@gmail.com>
2023-10-20 14:17:45 -0500

    3A

--- streamer.py
+++ streamer.py
@@ -3,6 +3,7 @@ from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
 import struct
+from concurrent.futures import ThreadPoolExecutor
 
 
 class Streamer:
@@ -17,6 +18,22 @@ class Streamer:
         self.seq = 0
         self.expecting = 0
         self.buffer = {}
+        self.closed = False
+
+        executor = ThreadPoolExecutor(max_workers=1)
+        executor.submit(self.listener)
+
+    def listener(self) :
+        while not self.closed: # a later hint will explain self.closed
+            try:
+                data, addr = self.socket.recvfrom()
+                header = data[:4]
+                body = data[4:]
+                seq = struct.unpack("<i", header)[0]
+                self.buffer[seq] = body
+            except Exception as e :
+                print("listener died!")
+                print(e)
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
@@ -29,27 +46,18 @@ class Streamer:
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
-        # your code goes here!  The code below should be changed! 
-        if self.expecting in self.buffer:
-            output = self.buffer[self.expecting]
-            del self.buffer[self.expecting]
-            self.expecting += len(output)
-            return output  
-            
+        # your code goes here!  The code below should be changed!
         while True:
-            data, addr = self.socket.recvfrom()
-            header = data[:4]
-            body = data[4:]
-            seq = struct.unpack("<i", header)[0]
-            if seq == self.expecting:
-                self.expecting += len(body)
-                return body
-            else:
-                self.buffer[seq] = body
+            if self.expecting in self.buffer:
+                output = self.buffer[self.expecting]
+                del self.buffer[self.expecting]
+                self.expecting += len(output)
+                return output  
 
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
         # your code goes here, especially after you add ACKs and retransmissions.
-        pass
+        self.closed = True
+        self.socket.stoprecv()
