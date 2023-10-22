054594d827274ac9dc59e8922e79a2ee577c64dd
jjb0123 <jjhbrice@gmail.com>
2023-10-22 14:29:47 -0500

    3C Finished
    Created a few more instance variables and finished up FIN ACKs

--- streamer.py
+++ streamer.py
@@ -8,6 +8,8 @@ import time
 
 
 class Streamer:
+    ACK_TIMEOUT=0.25
+    FIN_WAIT_TIMEOUT=2
     def __init__(self, dst_ip, dst_port,
                  src_ip=INADDR_ANY, src_port=0):
         """Default values listen on all network interfaces, chooses a random source port,
@@ -20,6 +22,8 @@ class Streamer:
         self.expecting = 0
         self.buffer = {}
         self.closed = False
+        self.ack=False
+        self.fin_received=False
 
         executor = ThreadPoolExecutor(max_workers=1)
         executor.submit(self.listener)
@@ -34,12 +38,15 @@ class Streamer:
                 type = header[1]
                 if type == 1:
                     self.ack = True
+                elif type == 2:
+                    self.fin_received=True
+                    newheader=struct.pack('<ii', 0, 1)
+                    self.socket.sendto(newheader, (self.dst_ip, self.dst_port))
                 else:
                     self.buffer[seq] = body
-                    print(self.buffer)
+                    # print(self.buffer)
                     newheader = struct.pack("<ii", self.seq, 1)
                     self.socket.sendto(newheader, (self.dst_ip, self.dst_port))
-
             except Exception as e :
                 print("listener died!")
                 print(e)
@@ -47,19 +54,19 @@ class Streamer:
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
         self.ack = False
+        chunks = [data_bytes[i:i+1464] for i in range(0, len(data_bytes), 1464)]
         # Your code goes here!  The code below should be changed!
-        initialseq = self.seq
-        while True:
-            self.seq = initialseq
-            for start in range(0, len(data_bytes), 1464):
+        # initialseq = self.seq
+        for chunk in chunks:
+            # self.seq = initialseq
+            while not self.ack:
                 header = struct.pack("<ii", self.seq, 0)
-                end = min(start + 1464, len(data_bytes))
-                self.socket.sendto(header + data_bytes[start:end], (self.dst_ip, self.dst_port))
-                self.seq += end - start
-            for i in range(25):
-                if self.ack:
-                    return
-                time.sleep(0.01)
+                self.socket.sendto(header + chunk, (self.dst_ip, self.dst_port))
+                time.sleep(self.ACK_TIMEOUT)
+            self.seq += len(chunk)
+            self.ack=False
+
+            
         
 
     def recv(self) -> bytes:
@@ -77,5 +84,13 @@ class Streamer:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
         # your code goes here, especially after you add ACKs and retransmissions.
+        while not self.ack:
+            fin_header=struct.pack("<ii", 0, 2)
+            self.socket.sendto(fin_header, (self.dst_ip, self.dst_port))
+            time.sleep(self.ACK_TIMEOUT)
+        while not self.fin_received:
+            time.sleep(0.01)
+        time.sleep(self.FIN_WAIT_TIMEOUT)
+
         self.closed = True
         self.socket.stoprecv()
