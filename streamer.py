21a12afabc3cad91bcb18361b1f205a468dbf359
Jordan Checkoff <jordan.checkoff@gmail.com>
2023-10-23 14:23:02 -0500

    Part 4

--- streamer.py
+++ streamer.py
@@ -5,13 +5,14 @@ from socket import INADDR_ANY
 import struct
 from concurrent.futures import ThreadPoolExecutor
 import time
+import hashlib
 
 
 class Streamer:
     ACK_TIMEOUT=0.25
     FIN_WAIT_TIMEOUT=2
 
-    DATA_CHUNK_SIZE=1464
+    DATA_CHUNK_SIZE=1448
 
     def __init__(self, dst_ip, dst_port,
                  src_ip=INADDR_ANY, src_port=0):
@@ -35,8 +36,10 @@ class Streamer:
         while not self.closed: # a later hint will explain self.closed
             try:
                 data, addr = self.socket.recvfrom()
+                if not data:
+                    break
                 header = struct.unpack("<ii", data[:8])
-                body = data[8:]
+                body = data[24:]
                 seq = header[0]
                 type = header[1]
                 if type == 1:
@@ -46,11 +49,12 @@ class Streamer:
                     newheader=struct.pack('<ii', 0, 1)
                     self.socket.sendto(newheader, (self.dst_ip, self.dst_port))
                 else:
-                    self.buffer[seq] = body
-                    # print(self.buffer)
-                    newheader = struct.pack("<ii", self.seq, 1)
-                    self.socket.sendto(newheader, (self.dst_ip, self.dst_port))
-            except Exception as e :
+                    hash = data[8:24]
+                    if hash == hashlib.md5(body).digest():
+                        self.buffer[seq] = body
+                        newheader = struct.pack("<ii", self.seq, 1)
+                        self.socket.sendto(newheader, (self.dst_ip, self.dst_port))
+            except Exception as e:
                 print("listener died!")
                 print(e)
 
@@ -58,12 +62,11 @@ class Streamer:
         """Note that data_bytes can be larger than one packet."""
         chunks = [data_bytes[i:i+self.DATA_CHUNK_SIZE] for i in range(0, len(data_bytes), self.DATA_CHUNK_SIZE)]
         # Your code goes here!  The code below should be changed!
-        # initialseq = self.seq
         for chunk in chunks:
             self.ack=False
-            # self.seq = initialseq
             while not self.ack:
-                header = struct.pack("<ii", self.seq, 0)
+                hash = hashlib.md5(chunk).digest()
+                header = struct.pack("<ii", self.seq, 0) + hash
                 self.socket.sendto(header + chunk, (self.dst_ip, self.dst_port))
                 time.sleep(self.ACK_TIMEOUT)
             self.seq += len(chunk)
@@ -87,18 +90,16 @@ class Streamer:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
         # your code goes here, especially after you add ACKs and retransmissions.
-        start_time=time.time()
 
-        while not self.ack and (time.time() - start_time) < self.ACK_TIMEOUT:
-            FIN_HEADER=-1
-            header=struct.pack("<ii", FIN_HEADER, 2)
+        self.ack = False
+        while not self.ack:
+            header=struct.pack("<ii", self.seq, 2)
             self.socket.sendto(header, (self.dst_ip, self.dst_port))
             time.sleep(self.ACK_TIMEOUT)
 
-        start_time=time.time()
-
-        while not self.fin_received and (time.time() -start_time) < self.FIN_WAIT_TIMEOUT:
+        while not self.fin_received:
             time.sleep(0.01)
 
-        self.closed = True
-        self.socket.stoprecv()
+        time.sleep(2)
+
+        self.closed = True and self.socket.stoprecv()
